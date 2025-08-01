f1641633586a6e5f959b3b947cfca6a2ef37e3cf
the-ujjawal <uksah123123@gmail.com>
2025-08-01 19:59:54 +0530

    Printing different patterns with digits

--- 8.LOOPS/Patterns.java
+++ 8.LOOPS/Patterns.java
@@ -15,7 +15,9 @@ public class Patterns {
         // for (int i = 1; i <= 5; i++) {
         //     for (int j = 1; j <= 5; j++) {
         //         System.out.print(i+j+" ");
-                
+
+
+        //PATTERN : 4     
         int count = 0;
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
